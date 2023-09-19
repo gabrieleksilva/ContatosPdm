a5f0533d1d1cc545c5cc3d278b023282131d3ef9
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2023-09-19 20:27:55 -0300

    renderizando a activity

--- app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/view/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.edu.scl.ifsp.ads.contatospdm.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.edu.scl.ifsp.ads.contatospdm.R
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.scl.ifsp.ads.contatospdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
