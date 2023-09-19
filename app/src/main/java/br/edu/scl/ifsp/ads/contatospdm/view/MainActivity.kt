0681ba54bd0e49661a91f3416d136a93603f209a
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2023-09-19 20:24:59 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.scl.ifsp.ads.contatospdm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.scl.ifsp.ads.contatospdm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
